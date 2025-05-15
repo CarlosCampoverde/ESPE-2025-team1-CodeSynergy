4bcdaa98ccf8085ce70ac8dd31e91fa2f01ce8f7
Niño Moi <benalcazarsebas8173@gmail.com>
2025-05-15 18:20:04 -0500

    create transporte

--- PHP/log_out.php
+++ PHP/log_out.php
@@ -3,6 +3,13 @@
     session_unset();   // Elimina todas las variables de la sesion
     session_destroy(); // Destruye 
 
+    if (session_status() == PHP_SESSION_NONE) {
+        echo "<script>
+                alert('Usuario cerró sesión correctamente.');
+                window.location.href = '../index.php';
+              </script>";
+    }   
+    
     header('Location: ../index.php'); // Redirige al inicio
     exit;
 ?>
