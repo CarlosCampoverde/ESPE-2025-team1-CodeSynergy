2fd62437239c9a13b20cb719d9033132240fb829
Carlos Danny Campoverde Encarnación <cdcampoverde@espe.edu.ec>
2025-05-16 17:54:06 -0500

    Update singut_status.php

--- PHP/signup_status.php
+++ PHP/signup_status.php
@@ -25,6 +25,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             ':password' => $password,
             ':email'    => $email
         ]);
+        //comprobar que el username o el correo no existan
+        $sql = "SELECT * FROM users WHERE username = :username OR email = :email";  
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([
+            ':username' => $username,
+            ':email'    => $email
+        ]);
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($user) {
+            $msg = json_encode("El nombre de usuario o el correo electrónico ya están en uso.");
+            echo "<script>
+                alert($msg);
+                window.location.href = '../index.php';
+            </script>";
+            exit;
+        }
 
         echo "<script>
             alert('Usuario registrado correctamente.');
