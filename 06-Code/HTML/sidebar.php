0218496aa4179daf15784214f2442742741addd7
Niño Moi <benalcazarsebas8173@gmail.com>
2025-05-23 22:26:31 -0500

    navigation bar

---
+++ 06-Code/HTML/sidebar.php
@@ -0,0 +1,53 @@
+<?php
+// Determinar la página actual (pasa este valor desde la página que incluye sidebar.php)
+$currentPage = isset($currentPage) ? $currentPage : '';
+?>
+<div class="sidebar-wrapper">
+    <ul class="sidebar-nav">
+        <li class="sidebar-brand">
+            <a href="#">Bienvenido Administrador</a>
+            <hr class="hr">
+        </li>
+        <br />
+        <li>
+            <a href="dashboard.php" class="<?php echo $currentPage === 'dashboard' ? 'active' : ''; ?>">
+                <i class="fa fa-th sidebar-icon"></i> Aplicación
+            </a>
+        </li>
+        <li>
+            <a href="quote-form.php" class="<?php echo $currentPage === 'quote-form' ? 'active' : ''; ?>">
+                <i class="fa fa-calendar-check-o sidebar-icon"></i> Cotización
+            </a>
+        </li>
+        <li>
+            <a href="clients.php" class="<?php echo $currentPage === 'clients' ? 'active' : ''; ?>">
+                <i class="fa fa-users sidebar-icon"></i> Clientes
+            </a>
+        </li>
+        <li>
+            <a href="menu.php" class="<?php echo $currentPage === 'menu' ? 'active' : ''; ?>">
+                <i class="fa fa-file sidebar-icon"></i> Menú
+            </a>
+        </li>
+        <li>
+            <a href="menu_items.php" class="<?php echo $currentPage === 'menu_items' ? 'active' : ''; ?>">
+                <i class="fa fa-file sidebar-icon"></i> Items del Menú
+            </a>
+        </li>
+        <li>
+            <a href="notifications.php" class="<?php echo $currentPage === 'notifications' ? 'active' : ''; ?>">
+                <i class="fa fa-bell sidebar-icon"></i> Notificaciones
+            </a>
+        </li>
+        <li>
+            <a href="employees.php" class="<?php echo $currentPage === 'employees' ? 'active' : ''; ?>">
+                <i class="fa fa-user sidebar-icon"></i> Empleados
+            </a>
+        </li>
+        <li>
+            <a href="../php/log_out.php" class="<?php echo $currentPage === 'log_out' ? 'active' : ''; ?>">
+                <i class="fa fa-sign-out-alt sidebar-icon"></i> Cerrar sesión
+            </a>
+        </li>
+    </ul>
+</div>
